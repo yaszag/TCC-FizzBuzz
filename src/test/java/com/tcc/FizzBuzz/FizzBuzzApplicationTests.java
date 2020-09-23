d55a8d6b28b6bdf62f1be3e53e091f5caa460c4d
yassin zaghdoudi <yassinzaghdoudi@gmail.com>
2020-09-24 01:57:45 +0200

    firstCommit

---
+++ src/test/java/com/tcc/FizzBuzz/FizzBuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.FizzBuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzBuzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
